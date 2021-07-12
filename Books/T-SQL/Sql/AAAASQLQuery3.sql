3f2fbc8afb81d35f1fb6c00392ce7f3b3f7cbd1a
czainski <38532082+czainski@users.noreply.github.com>
2021-07-12 16:50:59 +0200

    V1

---
+++ Books/T-SQL/Sql/AAAASQLQuery3.sql
@@ -0,0 +1,8 @@
+﻿create table xxx  (id1 int, Id2 int);
+insert  into xxx values (1, 2) ;
+insert  into xxx values (2, 2) ;
+insert  into xxx values (3, 20) ;
+insert  into xxx values (4, 20) ;
+insert  into xxx values (5, 20) ;
+
+
